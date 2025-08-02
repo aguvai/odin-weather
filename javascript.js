19fa0083ca2cb7c752938a19764823d4a7f15adf
aguvai <145935152+aguvai@users.noreply.github.com>
2025-08-01 23:55:40 -0400

    Add async json data processing option

--- javascript.js
+++ javascript.js
@@ -1,12 +1,45 @@
 const key = "M968YXWKY66KWX9TW36NPJSQK";
+let location = "hyde park, ny"
 
-const data = fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/poughkeepsie?key=${key}&include=current`, { mode: 'cors', })
-    .then((response) => {
-        return response.json()
-    })
-    .then((response) => {
-        console.log(response)
-    })
-    .catch(function (err) {
-        console.log(err)
-    });;
+const option = "async";
+
+if (option === "promises") {
+    console.log("promises chosen")
+    
+    const data = fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${key}&include=current`, { mode: 'cors', })
+        .then((response) => {
+            return response.json()
+        })
+        .then((data) => {
+            console.log(`
+            ${data.resolvedAddress}:
+
+            Current time: ${data.currentConditions.datetime},
+            
+            Temperature: ${data.currentConditions.temp}
+            Feels like: ${data.currentConditions.feelslike}
+
+            Humidity: ${data.currentConditions.humidity}
+            `)
+        })
+        .catch(function (err) {
+            console.log(err)
+        });;
+} else if (option === "async") {
+    console.log("async chosen")
+
+    getWeather = async function () {
+        const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${key}&include=current`, { mode: 'cors', })
+        const data = await response.json();
+        console.log(`
+            ${data.resolvedAddress}:
+
+            Current time: ${data.currentConditions.datetime},
+            
+            Temperature: ${data.currentConditions.temp}
+            Feels like: ${data.currentConditions.feelslike}
+
+            Humidity: ${data.currentConditions.humidity}
+            `)
+    }()
+}
